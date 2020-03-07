28f2e6deb6566ecf9788badc85b73d8a564e47d6
Yuchen Liu <eliu0615@gmail.com>
2020-03-07 12:39:33 -0500

    fix

--- login-portal/app.js
+++ login-portal/app.js
@@ -1,8 +1,8 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger = require('morgan'); //http request logger (records every http requests)
+var session = require("express-session"); //don't need to login on every page
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,9 +16,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
